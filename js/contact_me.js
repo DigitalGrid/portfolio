8a086bb8a4324e223d4dfa934dd12c74026022d6
DigitalGrid <christofer.jadelius@gmail.com>
2016-05-14 12:38:24 +0200

    form fix

--- js/contact_me.js
+++ js/contact_me.js
@@ -42,7 +42,8 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
+                error: function(response) {
+                    alert(response);
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
